56f69dfcda3aa4851a46e3f5785ae99168d3d51f
houssemguesmi <houssemguesmi14@gmail.com>
2020-11-08 16:31:18 +0100

    initr

--- queue.js
+++ queue.js
@@ -1,36 +1,34 @@
 class Queue {
-    constructor() {
-        this.front = 0;
-        this.rear = 0;
-        this.storage = {};
-    }
+  constructor() {
+    this.front = 0;
+    this.rear = 0;
+    this.storage = {};
+  }
 
-    enqueue(value) {
-        if(this.rear === 0) {
-            this.storage[this.front] = value;
-            this.rear++;
-        } else {
-            this.storage[this.rear] = value;
-            this.rear++;
-        }
+  enqueue(value) {
+    if (this.rear === 0) {
+      this.storage[this.front] = value;
+      this.rear++;
+    } else {
+      this.storage[this.rear] = value;
+      this.rear++;
     }
-    dequeue() {
-        let deletedItem = this.storage[this.front]
-        delete this.storage[this.front]
-        for(var key in this.storage) {
-            this.storage[key-1] = this.storage[key]
-        }
-        delete this.storage[this.rear-1]
-        if(this.rear > 0) {
-            this.rear--;
-        }
-        return deletedItem;
+  }
+  dequeue() {
+    if (this.storage[this.front]) {
+      let deletedItem = this.storage[this.front];
+      delete this.storage[this.front];
+      this.front++;
+      return deletedItem;
+    } else {
+      return "You don't have any value to dequeue!";
     }
-    size() {
-        if(this.rear === 0) {
-            return 0;
-        } else {
-            return this.rear;
-        }
+  }
+  size() {
+    if (this.rear === 0) {
+      return 0;
+    } else {
+      return this.rear;
     }
+  }
 }
