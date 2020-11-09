f4cd64b18f8b32fe566391717804220eecd75000
houssemguesmi <houssemguesmi14@gmail.com>
2020-11-09 09:47:35 +0100

    init

--- stack.js
+++ stack.js
@@ -9,12 +9,16 @@ class Stack {
     this.top++;
   }
   pop() {
-    let deletedItem = this.storage[this.top - 1];
-    delete this.storage[this.top - 1];
-    if (this.top > 0) {
-      this.top--;
+    if (this.storage[this.top - 1]) {
+      let deletedItem = this.storage[this.top - 1];
+      delete this.storage[this.top - 1];
+      if (this.top > 0) {
+        this.top--;
+      }
+      return deletedItem;
+    } else {
+      return "You don't have any value to pop!";
     }
-    return deletedItem;
   }
   size() {
     if (this.top === 0) {
